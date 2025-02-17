3a371e787ab7d2816465c293706c72d04eab9230
no-milk-today <drmhere@yandex.by>
2025-02-17 14:08:15 +0300

    #webapp 2d part (users template)

--- src/main/java/com/yandex/blog/controller/UserController.java
+++ src/main/java/com/yandex/blog/controller/UserController.java
@@ -1,15 +1,29 @@
 package com.yandex.blog.controller;
 
+import com.yandex.blog.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/users") // Контроллер обрабатывает запросы /users
 public class UserController {
 
-    @GetMapping // GET-запрос /users
-    public String users() {
+    @GetMapping("/users")
+    public String users(Model model) {
+        // Данные теперь получаются программно
+        List<User> users = Arrays.asList(
+                new User(1L, "Иван", "Иванов", 30, true),
+                new User(2L, "Пётр", "Петров", 25, false),
+                new User(3L, "Мария", "Сидорова", 28, true)
+        );
+        // Передаём данные в виде атрибута users
+        model.addAttribute("users", users);
+
         return "users"; // Возвращаем название шаблона — users.html
     }
 
